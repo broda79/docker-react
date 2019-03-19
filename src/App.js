73cc8a6f2b158af9bf7992c8be00ef169c015804
rafał <rafalkaska@gmail.com>
2019-03-19 19:36:15 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Zmieniłem co nieco
           </a>
         </header>
       </div>
